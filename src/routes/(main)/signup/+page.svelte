c092e9cf7a4395dc7ea7890078fef1851415e71c
dev-henen <henen.dev@gmail.com>
2024-12-12 13:53:39 +0100

    Created login and signup pages

--- src/routes/(main)/signup/+page.svelte
+++ src/routes/(main)/signup/+page.svelte
@@ -2,41 +2,33 @@
 </script>
 
 <svelte:head>
-    <title>Login - Monierate</title>
+    <title>Sign up - Monierate</title>
 </svelte:head>
 
-<div class="pt-5 flex items-center justify-center">
+<div
+    class="pt-5 flex items-center justify-center"
+>
     <div
         class="bg-white dark:bg-gray-900 w-full max-w-md rounded-lg shadow-lg p-6 relative"
     >
         <div
             class="flex justify-between mb-4 bg-gray-100 dark:bg-gray-800 rounded-lg p-2"
         >
-            <a
-                href="/signup"
-                class="button bg-transparent hover:bg-gray-200 dark:hover:bg-gray-700 text-dark dark:text-light text-center w-1/2 font-semibold"
-            >
+            <a href="/signup" class="button text-center w-1/2 font-semibold">
                 Sign Up
             </a>
-            <a href="/login" class="button text-center w-1/2 font-semibold">
+            <a
+                href="/login"
+                class="button bg-transparent hover:bg-gray-200 dark:hover:bg-gray-700 text-dark dark:text-light text-center w-1/2 font-semibold"
+            >
                 Login
             </a>
         </div>
 
         <div class="py-2">
-            <img
-                src="/favicon.png"
-                alt="Monierate"
-                width="35"
-                class="inline dark:hidden"
-            />
-            <img
-                src="/favicon-white.png"
-                alt="Monierate"
-                width="35"
-                class="hidden dark:inline"
-            />
-            <h2 class="text-xl font-semibold mb-4 inline">Login</h2>
+            <img src="/favicon.png" alt="Monierate" width="35" class="inline dark:hidden"/>
+            <img src="/favicon-white.png" alt="Monierate" width="35" class="hidden dark:inline"/>
+            <h2 class="text-xl font-semibold mb-4 inline">Sign Up</h2>
         </div>
 
         <p class="text-sm text-gray-600 mb-4">
@@ -74,7 +66,19 @@
                 </p>
             </div>
 
-            <button type="submit" class="button w-full"> Login </button>
+            <div>
+                <label class="label" for="confirm-login-password"
+                    >Confirm Password</label
+                >
+                <input
+                    type="password"
+                    class="input"
+                    id="confirm-login-password"
+                    placeholder="Confirm Password"
+                />
+            </div>
+
+            <button type="submit" class="button w-full"> Sign up </button>
         </form>
 
         <div
